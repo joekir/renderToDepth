20820b14dbe4191c60846bc686b1c7265e200177
Joe Kirwin <mail@josephkirwin.com>
2018-04-25 21:17:18 -0700

    support Macs :(

--- renderToDepth.py
+++ renderToDepth.py
@@ -1,28 +1,42 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from subprocess import call
 import argparse
 import fileinput
 from os import remove
+import platform
 import re
-from shutil import move
+import shutil
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file", help="UML file name")
-parser.add_argument("-d","--depth",help="Anything after this depth will not be rendered")
+parser.add_argument("file", type=str, help="UML file name")
+parser.add_argument("-d","--depth", type=int, help="Anything after this depth will not be rendered", required=True)
 
 args = parser.parse_args()
+
+if not args.depth > 0:
+    print("\n\tDepth must be greater than zero\n")
+    exit(1)
 
 for line in fileinput.input(args.file, inplace=1, backup='.bak'):
     line = re.sub('^\\t{{{}}}'.format(args.depth),"'", line.rstrip())
     print(line)
 
+if not shutil.which("plantuml"):
+    print("\n\tPlantUML not found, please install!\n")
+    exit(1)
+
 call(["plantuml", args.file])
 image = args.file.rsplit(".", 1)[0] + '.png'
-call(["eog",image])
+
+pl = platform.system()
+if pl == "Linux":
+    call(["eog", image])
+elif pl == 'Darwin':
+    call(["open", image])
 
 # restore original file
-move(args.file + '.bak', args.file)
+shutil.move(args.file + '.bak', args.file)
 
 # cleanup png
 remove(image)
